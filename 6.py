d9cb8feeb24b6df040af2359a8422da06443b8ae
MoriMotonari <finn.harbeke@gmail.com>
2021-12-06 06:27:59 +0100

    day 6

---
+++ 6.py
@@ -0,0 +1,13 @@
+fish = [int(x) for x in input().split(",")]
+nWithTime = [0] * 9
+for x in fish:
+    nWithTime[x] += 1
+for t in range(256):
+    nWithTime = nWithTime[1:] + [nWithTime[0]]
+    nWithTime[6] += nWithTime[8]
+    print(nWithTime)
+print(sum(nWithTime))
+
+
+#print(fish)
+print(len(fish))
