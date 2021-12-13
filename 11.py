393c8366345b7947cbeb641e63bba401998e393c
MoriMotonari <finn.harbeke@gmail.com>
2021-12-13 05:59:10 +0100

    clean up 11.py

--- 11.py
+++ 11.py
@@ -1,5 +1,4 @@
 m1 = [[int(x) for x in input()] for i in range(10)]
-# m2 = m1.copy()
 flashed = set()
 f = 0
 
@@ -15,50 +14,17 @@ def flash(i, j):
             if m1[i+ni][j+nj] > 9:
                 flash(i+ni, j+nj)
 
-# for i in range(len(m1)):
-#     for j in range(len(m1[i])):
-#         if (i, j) in flashed:
-#             print("_", end="")
-#         else:
-#             print(" ", end='')
-#     print()
-#     for j in range(len(m1[i])):
-#         print(m1[i][j], end="")
-
-#     print()
-# print()
-
-for s in range(2000):
+for s in range(500):
     flashed = set()
     for i in range(len(m1)):
         for j in range(len(m1[i])):
             m1[i][j] += 1
-    # m2 = m1.copy()
-    for i in range(len(m1)):
-        for j in range(len(m1[i])):
-            if m1[i][j] <= 9:
-                continue
-            else:
+            if m1[i][j] > 9:
                 flash(i, j)
-
     for (i, j) in flashed:
         m1[i][j] = 0
-
-    # for i in range(len(m1)):
-    #     for j in range(len(m1[i])):
-    #         if (i, j) in flashed:
-    #             print("_", end="")
-    #         else:
-    #             print(" ", end='')
-    #     print()
-    #     for j in range(len(m1[i])):
-    #         print(m1[i][j], end="")
-            
-    #     print()
-    # print()
     if s == 99:
         print(f)
     if len(flashed) == len(m1) * len(m1[0]):
         print(s+1)
         break
-    
