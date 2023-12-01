b73b107fa76826753ecf9b227475c680a49bf6da
finnharbeke <code@finnharbeke.ch>
2023-12-01 10:10:18 +0100

    c++ template

---
+++ 2023/template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define int int64_t
+using namespace std;
+
+const bool OO = 1;
+#define p(x) if (OO) { cout << #x << ": " << x << endl;}
+#define p_vec(x) if (OO) { cout << #x << ": "; for (auto val: x) { cout << val << " "; }; cout << endl;}
+#define p_2dvec(x) if (OO) { cout << #x << ":\n"; for (auto v: x) { for (auto val: v) cout << val << " "; cout << endl; }}
+
+signed main() {
+
+    cout << res << endl;
+    return 0;
+}
