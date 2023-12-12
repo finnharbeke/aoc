a5617182593a034abcfe252ce4a43243cfb9884a
finnharbeke <code@finnharbeke.ch>
2023-12-12 14:48:41 +0100

    2023 8

--- 2023/8.cpp
+++ 2023/8.cpp
@@ -8,6 +8,20 @@ const bool OO = 1;
 #define p_vec(x) if (OO) { cout << #x << ": "; for (auto val: x) { cout << val << " "; }; cout << endl;}
 #define p_2dvec(x) if (OO) { cout << #x << ":\n"; for (auto v: x) { for (auto val: v) cout << val << " "; cout << endl; }}
 
+bool compare(pair<int, int> q_part1, pair<int, int> q_part2) {
+    return q_part1.first < q_part2.first;
+}
+
+int gcd(int a, int b) {
+  int R;
+  while ((a % b) > 0) {
+    R = a % b;
+    a = b;
+    b = R;
+  }
+  return b;
+}
+
 signed main() {
     string instrs;
     cin >> instrs;
@@ -31,14 +45,14 @@ signed main() {
 
     string pos = "AAA";
     int step = 0;
-    // while (pos != "ZZZ") {
-    //     if (instrs.at(step % instrs.size()) == 'L')
-    //         pos = camelmap.at(pos).first;
-    //     else
-    //         pos = camelmap.at(pos).second;
-    //     step++;
-    // }
-    // cout << step << endl;
+    while (pos != "ZZZ") {
+        if (instrs.at(step % instrs.size()) == 'L')
+            pos = camelmap.at(pos).first;
+        else
+            pos = camelmap.at(pos).second;
+        step++;
+    }
+    cout << step << endl;
     step = 0;
     bool allZ = false;
     while (!allZ) {
@@ -56,5 +70,83 @@ signed main() {
         step++;
     }
     cout << step << endl;
+    vector<int> cycle_lens;
+    vector<pair<int, int>> queue;
+    for (int g = 0; g < ghosts.size(); g++) {
+        step = 0;
+        vector<pair<string, int>> path;
+        string current = ghosts.at(g);
+        bool been_there = false;
+        while (!been_there) {
+            for (auto before : path)
+                if (before.first == current && before.second == step % instrs.size())
+                    been_there = true;
+            path.push_back(pair<string, int>{current, step % instrs.size()});
+            if (instrs.at(step % instrs.size()) == 'L')
+                current = camelmap.at(current).first;
+            else
+                current = camelmap.at(current).second;
+            step++;
+        }
+        if (OO && false) {
+            for (auto step : path)
+                cout << step.first << ";" << step.second << " ";
+            cout << endl;
+        }
+
+        
+        int cycle_enter = find(path.begin(), path.end(), path.at(path.size()-1)) - path.begin();
+        int cycle_len = path.size() - 1 - cycle_enter;
+        cycle_lens.push_back(cycle_len);
+        for (int i = cycle_enter; i < path.size() - 1; i++) {
+            if (path.at(i).first.at(2) == 'Z')
+                queue.push_back(pair<int,int>{i, g});
+        }
+        p(cycle_enter)
+        p(cycle_len)
+        // int cycle_len = path.size() - 
+    }
+
+    // bool same_step = false;
+    // sort(queue.begin(), queue.end(), compare);
+    // int round = 0;
+    // step = queue.at(0).first;
+    // while (!same_step) {
+    //     if (round % 1000 == 0) {
+    //         for (auto thing : queue)
+    //             cout << thing.first << "(" << thing.second << ") ";
+    //         cout << endl;
+    //     }
+    //     pair<int, int> next = queue.at(0);
+    //     queue.erase(queue.begin());
+    //     queue.push_back(pair<int, int>{ step + cycle_lens.at(next.second), next.second });
+    //     sort(queue.begin(), queue.end(), compare);
+    //     same_step = true;
+    //     step = queue.at(0).first;
+    //     for (int i = 0; i < ghosts.size() && same_step; i++) {
+    //         bool found = false;
+    //         for (int j = 0; j < queue.size(); j++)
+    //             if (queue.at(j).second == i && queue.at(j).first == step)
+    //                 found = true;
+    //         if (!found)
+    //             same_step = false;
+    //     }
+    //     round++;
+    // }
+    // cout << step << endl;
+
+    // this is so stupid, lcm of cycle_lens works
+    int n1 = cycle_lens.at(0);
+    int n2;
+    int ix = 1;
+    int lcm = n1;
+    while (ix < cycle_lens.size()) {
+        n1 = lcm;
+        n2 = cycle_lens.at(ix);
+        ix++;
+        int g = gcd(n1, n2);
+        lcm = (int)(n1*n2/g);
+        p(lcm);
+    }
     return 0;
 }
